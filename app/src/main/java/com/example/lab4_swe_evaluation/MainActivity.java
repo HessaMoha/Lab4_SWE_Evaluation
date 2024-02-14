f4b662aa125ea71ad0660d9fdcccc119b8532b32
HessaMoha <hessaalajaleen@gmail.com>
2024-02-14 09:26:51 +0300

    step2

--- app/src/main/java/com/example/lab4_swe_evaluation/MainActivity.java
+++ app/src/main/java/com/example/lab4_swe_evaluation/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.lab4_swe_evaluation;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,25 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button send_button;
-    EditText send_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        send_button = findViewById(R.id.send_button_id);
-        send_text = findViewById(R.id.send_text_id);
-
-        send_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String str = send_text.getText().toString();
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                intent.putExtra("message_key", str);
-                startActivity(intent);
-            }
-        });
     }
 }
